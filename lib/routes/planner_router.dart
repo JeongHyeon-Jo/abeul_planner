3037e8b5f94906854ee279c8a62afc7a15978e7a
JeongHyeon Jo <abeul25@naver.com>
2025-04-03 20:22:23 +0900

    [Feat] Planner 라우팅 추가

--- lib/routes/planner_router.dart
+++ lib/routes/planner_router.dart
@@ -1,6 +1,11 @@
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+// planner
 import 'package:abeul_planner/features/home_planner/presentation/screen/planner_home_screen.dart';
+import 'package:abeul_planner/features/calendar_planner/presentation/screen/calendar_planner_sceen.dart';
+import 'package:abeul_planner/features/daily_planner/presentation/screen/daily_planner_sceen.dart';
+import 'package:abeul_planner/features/weekly_planner/presentation/screen/weekly_planner_sceen.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/home',
@@ -9,6 +14,21 @@ final GoRouter router = GoRouter(
       path: '/home',
       builder: (context, state) => const PlannerHomeScreen(),
     ),
-    // 여기에 다른 플래너 화면들 추가 예정 (ex: 일상, 요일, 달력 등)
+    GoRoute(
+      path: 'daily',
+      builder: (context, state) => const DailyPlannerScreen(),
+    ),
+    GoRoute(
+      path: 'weekly',
+      builder: (context, state) => const WeeklyPlannerScreen(),
+    ),
+    GoRoute(
+      path: 'calendar',
+      builder: (context, state) => const CalendarPlannerScreen(),
+    ),
+    GoRoute(
+      path: 'settings',
+      builder: (context, state) => const Placeholder(), // 추후 설정 화면 교체
+    ),
   ],
 );
