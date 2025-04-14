1f2154373405a427e3bb2d80d58df5c1f2fcefaf
JeongHyeon Jo <abeul25@naver.com>
2025-04-14 22:17:53 +0900

    [Design] Settings_Screen 디자인 개선

---
+++ lib/features/settings/presentation/widget/settings_tile.dart
@@ -0,0 +1,29 @@
+// settings_tile.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:abeul_planner/core/text_styles.dart';
+
+/// 설정 항목 개별 타일 위젯
+class SettingsTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final VoidCallback? onTap;
+
+  const SettingsTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: 16.w),
+      leading: Icon(icon, size: 24.sp),
+      title: Text(title, style: AppTextStyles.body),
+      trailing: Icon(Icons.arrow_forward_ios, size: 16.sp),
+      onTap: onTap,
+    );
+  }
+}
